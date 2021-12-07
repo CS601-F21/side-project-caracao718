8b293a7d037c4878b1c34e0468a02ac42f0976ca
caracao718 <caracaoengineer@gmail.com>
2021-12-07 10:09:59 +0800

    add maven dependency, such as springboot2

--- src/main/java/com/caracao718/MainApplication.java
+++ src/main/java/com/caracao718/MainApplication.java
@@ -1,10 +1,14 @@
 package com.caracao718;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * springboot main entrance class
  */
+@SpringBootApplication
 public class MainApplication {
-    public static void main(String[] args){  
-        
+    public static void main(String[] args) {
+        SpringApplication.run(MainApplication.class, args);
     }
 }
