ba02e11921f5b55ea7565304660a9bc0b71a40fd
caracao718 <caracaoengineer@gmail.com>
2021-12-09 10:43:24 +0800

    remove token(github check it's not secret)

--- src/main/java/com/caracao718/service/WeatherUpdate.java
+++ src/main/java/com/caracao718/service/WeatherUpdate.java
@@ -2,10 +2,15 @@ package com.caracao718.service;
 
 import com.caracao718.domain.Mountain;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.List;
 
 /**
@@ -17,18 +22,36 @@ public class WeatherUpdate {
     RestTemplate template;
     @Autowired
     MountainService mountainService;
+    @Autowired
+    ResourceLoader resourceLoader;
 
     /**
      * Daily weather updates
      */
     @Scheduled(initialDelay=86400000, fixedDelay=86400000)
     public void update(){
+        String token = readToken();
         List<Mountain> list = mountainService.list();
         for (Mountain mountain : list) {
-            String url = "https://api.openweathermap.org/data/2.5/weather?zip="+mountain.getZipCode()+",US&appid=fc66eba0fd6733614ef4e5ca5bcb36e0";
+            String url = "https://api.openweathermap.org/data/2.5/weather?zip="+mountain.getZipCode()+",US&appid="+token;
             String weather = template.getForObject(url, String.class);
             mountainService.updateWeather(mountain.getId(), weather);
         }
         System.out.println("update finish");
+    }
+
+    private String readToken(){
+        Resource resource = resourceLoader.getResource("classpath:token.txt");
+        try {
+            InputStreamReader isr = new InputStreamReader(resource.getInputStream());
+            BufferedReader br = new BufferedReader(isr);
+            String data;
+            while((data = br.readLine()) != null) {
+                return data;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return "";
     }
 }
