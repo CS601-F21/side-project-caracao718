3cc009b72c676286e1363ec08d4930286686bbc6
caracao718 <caracaoengineer@gmail.com>
2021-12-09 09:30:02 +0800

    Add http tool for access weather json

--- src/main/java/com/caracao718/MainApplication.java
+++ src/main/java/com/caracao718/MainApplication.java
@@ -1,8 +1,12 @@
 package com.caracao718;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * springboot main entrance class
@@ -10,6 +14,14 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class MainApplication {
+    @Autowired
+    private RestTemplateBuilder builder;
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return builder.build();
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
     }
