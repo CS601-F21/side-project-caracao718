f9d774b0a94bfad670fcc50484d47ed8196e484e
caracao718 <caracaoengineer@gmail.com>
2021-12-09 09:18:10 +0800

    open job scheduling

--- src/main/java/com/caracao718/MainApplication.java
+++ src/main/java/com/caracao718/MainApplication.java
@@ -2,11 +2,13 @@ package com.caracao718;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * springboot main entrance class
  */
 @SpringBootApplication
+@EnableScheduling
 public class MainApplication {
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
